a51b60652ed675e45bcfa6978f176ef5c4465376
kyzz339 <kyzz339@naver.com>
2021-02-07 02:05:07 +0900

    버튼수정

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -62,7 +62,7 @@ let project_mapper = project => {
                         ).join('')}
                     </div><br>
                     <div class="git_button">
-                    	<button type="button" class="semi-transparent-button1 with-border nav-links" onclick="location.href='${project.link}'">Contact</button>
+                    	<button type="button" class="semi-transparent-button1" onclick="location.href='${project.link}'">Contact</button>
                     </div>
                 </div>
             </div>
